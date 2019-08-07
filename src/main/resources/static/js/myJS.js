78b6be48a29275697ab88926015b61639705ff31
xiongipp <39366100+xiongipp@users.noreply.github.com>
2019-08-07 17:18:21 +0800

    nothing

--- src/main/resources/static/js/myJS.js
+++ src/main/resources/static/js/myJS.js
@@ -23,66 +23,87 @@ function collapseComments(e) {
     //移除高亮标签
     e.classList.remove("active");
   }else{
-    $.getJSON("/comment/"+id, function(data){
-      console.log(data);
-      var commentBody=$("comment-body-"+id)
-      var items=[];
-      commentBody.appendChild()
-      $.each(data.data, function(comment) {
-        $("<div/>",{
-          "class:":"col-lg-12 col-md-12 col-sm-12 col-xs-12 comments," ,
-          html: items.join('')
-        })
-        items.push('<li id="' + key + '">' + val + '</li>');
-      });
-      $("<div/>",{
-        "class:":"col-lg-12 col-md-12 col-sm-12 col-xs-12 collapse sub-comments," ,
-        "id":"comment-"+id,
-        html: items.join('')
-      }).appendTo(commentBody);
-
-    //展开二级评论
-    comments.addClass("in");
-    //标记评论展开状态
-    e.setAttribute("data-collapse","in");
-    //高亮评标签
-    e.classList.add("active");
-    });
+      var subCommentContainer=$("#comment-"+id);
+      if(subCommentContainer.children().length!=1){
+          //展开二级评论
+          comments.addClass("in");
+          //标记评论展开状态
+          e.setAttribute("data-collapse","in");
+          //高亮评标签
+          e.classList.add("active");
+      }else{
+          $.getJSON("/comment/"+id, function(data){
+          $.each(data.data.reverse(), function(index,comment) {
+              var mediaLeftElement=$("<div/>",{
+                  "class":"media-left"
+              }).append($("<img/>",{
+                  "class":"media-object img-rounded",
+                  "src":comment.user.avatarUrl
+              }))
+              var mediaBodyElement=$("<div/>",{
+                  "class":"media-body"
+              }).append($("<h5/>",{
+                  "class":"media-heading",
+                  "html":comment.user.name
+              })).append($("<div/>",{
+                  "html":comment.content
+              })).append($("<div/>",{
+                  "class":"menu"
+              }).append($("<span/>",{
+                  "class":"pull-right",
+                  "html":moment(comment.gmtCreate).format("YYYY-MM-DD")
+                  })))
+              var mediaElement=$("<div/>",{
+                  "class":"media"
+              }) .append(mediaLeftElement).append(mediaBodyElement);
+              var commentElement= $("<div/>",{
+                  "class:":"col-lg-12 col-md-12 col-sm-12 col-xs-12 comments" ,
+              }).append(mediaElement);
+              subCommentContainer.prepend(commentElement);
+          });
+    })
+          //展开二级评论
+          comments.addClass("in");
+          //标记评论展开状态
+          e.setAttribute("data-collapse","in");
+          //高亮评标签
+          e.classList.add("active");
   }
-  
+}
+
 }
 function comment2target(targetId,type,content){
-  if(!content){
-    alert("回复不能为空！")
-    return;
-  }
-  $.ajax({
-    type: "POST",
-    url: "/comment",
-    contentType:"application/json",
-    data:JSON.stringify({
-      "parentId": targetId,
-      "content": content,
-      "type": type,
-    }),
-      success: function (response) {
-        if(response.code==200){
-          window.location.reload();
-          $("#comment_section").hide();
-        }else {
-          if(response.code==2004){
-            var isAccepted=confirm(response.message);
-            if(isAccepted){
-              window.open("https://github.com/login/oauth/authorize?client_id=ce98d1e93a586201af03&redirect_uri=http://localhost:8087/callback&scope=user&state=1")
-              window.localStorage.setItem("closable",true);
-            }
-          }else{
-            alert(response.message());
-          }
+    if(!content){
+        alert("回复不能为空！")
+        return;
+    }
+    $.ajax({
+        type: "POST",
+        url: "/comment",
+        contentType:"application/json",
+        data:JSON.stringify({
+            "parentId": targetId,
+            "content": content,
+            "type": type,
+        }),
+        success: function (response) {
+            if(response.code==200){
+                window.location.reload();
+                $("#comment_section").hide();
+            }else {
+                if(response.code==2004){
+                    var isAccepted=confirm(response.message);
+                    if(isAccepted){
+                        window.open("https://github.com/login/oauth/authorize?client_id=ce98d1e93a586201af03&redirect_uri=http://localhost:8087/callback&scope=user&state=1")
+                        window.localStorage.setItem("closable",true);
+                    }
+                }else{
+                    alert(response.message());
+                }
 
-        }
-        console.log(response);
-      },
-      dataType:"json"
+            }
+            console.log(response);
+        },
+        dataType:"json"
     });
 }
